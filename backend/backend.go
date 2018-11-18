5226e864f0e2243d3ada96c9fd297d0a99c60bca
Jayson Isaac <jayson.isaac1@gmail.com>
2018-11-18 16:35:31 -0500

    Revert PR #5 to #14
    Graeme wrote sooo much code
    
    This reverts commit 32df86382f34ca9042f949503e017e7543793482, reversing
    changes made to d618031bf18f9d61c0454ab8ab70c1cf1125def6.
    
    Revert "Merge pull request #13 from jki127/gqo"
    
    This reverts commit d618031bf18f9d61c0454ab8ab70c1cf1125def6, reversing
    changes made to 208dade2c2f4431d91bce6bdb12d27452aacd7a1.
    
    Revert "Merge pull request #12 from jki127/gqo"
    
    This reverts commit 208dade2c2f4431d91bce6bdb12d27452aacd7a1, reversing
    changes made to 0ce48c507eec4f1721384a32205915dbe82e35b9.
    
    Revert "Merge pull request #11 from jki127/gqo"
    
    This reverts commit 0ce48c507eec4f1721384a32205915dbe82e35b9, reversing
    changes made to e0ff895632568f24dfefb594569d37d7143e0729.
    
    Revert "Merge pull request #10 from jki127/gqo"
    
    This reverts commit e0ff895632568f24dfefb594569d37d7143e0729, reversing
    changes made to c23fa747f1cc47e94e96ebb4023128b9d0a9bc94.
    
    Revert "Merge pull request #8 from jki127/gqo"
    
    This reverts commit c23fa747f1cc47e94e96ebb4023128b9d0a9bc94, reversing
    changes made to bf8a28f723ccc648514c2ea05300dc5c6e4cb831.
    
    Revert "Merge pull request #7 from jki127/gqo"
    
    This reverts commit bf8a28f723ccc648514c2ea05300dc5c6e4cb831, reversing
    changes made to 31c5a97f136f26099af2a9b9a1429fb1562e8d70.
    
    Revert "Merge pull request #5 from jki127/gqo"
    
    This reverts commit 31c5a97f136f26099af2a9b9a1429fb1562e8d70, reversing
    changes made to bc996cc570a522bea36af177c131e3428344e2be.

--- backend/backend.go
+++ backend/backend.go
@@ -1,136 +1,15 @@
-package backend
+// This is a placeholder backend .go file
+package main
 
 import (
-	"database/sql"
-	"encoding/json"
 	"log"
-	"os"
 
-	// Used to interact with mySQL DB
+	_ "database/sql"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
-// Conf holds configuration data for DSN
-type Conf struct {
-	User   string
-	Pass   string
-	DBName string
-}
-
-// ContentItem holds info of Content_Item entities in the database
-type ContentItem struct {
-	ItemID   int
-	Email    string
-	FilePath string
-	FileName string
-	PostTime string // should use go date format later
-}
-
-var db *sql.DB
-
-// TestDB tries to ping the database and returns the resulting error
-func TestDB() error {
-	return db.Ping()
-}
-
-/*
-GetPubContent queries DB for all Content_Item entities with a public
-status and returns them as an array of ContentItem pointers.
-*/
-func GetPubContent() []*ContentItem {
-	// Query DB for data
-	rows, err := db.Query(`SELECT * FROM Content_Item 
-		WHERE is_pub = true 
-		AND post_time >= DATE_SUB(NOW(), INTERVAL 24 HOUR)`)
-	if err != nil {
-		log.Println("Could not query public content from DB.")
-	}
-	defer rows.Close()
-
-	// Declare variables for processing data
-	var (
-		itemID      int
-		email       string
-		filePath    string
-		fileName    string
-		postTime    string
-		isPub       int
-		data        []*ContentItem
-		CurrentItem *ContentItem
-	)
-
-	for rows.Next() {
-		err = rows.Scan(&itemID, &email, &filePath, &fileName, &postTime, &isPub)
-		if err != nil {
-			log.Println("Could not scan row data from public content query.")
-		}
-		CurrentItem = &ContentItem{
-			ItemID:   itemID,
-			Email:    email,
-			FilePath: filePath,
-			FileName: fileName,
-			PostTime: postTime,
-		}
-		data = append(data, CurrentItem)
-	}
-
-	return data
-}
-
-/*
-ValidateInfo receives user entered login info and queries the DB on whether
-or not that info is valid
-*/
-func ValidateInfo(username string, password string) bool {
-	// Query DB for data
-	rows, err := db.Query(`SELECT email FROM Person
-		WHERE email=?
-		AND password=SHA2(?,256)`,
-		username, password)
-	if err != nil {
-		log.Println("Validate query statement failed.")
-	}
-
-	// Check if query returned 1 result (i.e. username and password matched entry)
-	var email string
-	count := 0
-	for rows.Next() {
-		err = rows.Scan(&email)
-		if err != nil {
-			log.Println("Could not scan row data from validate query")
-		}
-		count++
-	}
-
-	switch count {
-	case 1:
-		return true
-	case 0:
-		return false
-	default:
-		log.Println("Unexpected count in DB:", count)
-		return false
-	}
-}
-
-func init() {
-	var configData Conf
-
-	configFile, err := os.Open("../backend/config.json")
-	if err != nil {
-		log.Println("Could not open config file.")
-	}
-	decoder := json.NewDecoder(configFile)
-	err = decoder.Decode(&configData)
-	if err != nil {
-		log.Println("Could not decode config file.")
-	}
-
-	dSN := configData.User + ":" + configData.Pass + "@/" + configData.DBName
-	log.Println(dSN)
-
-	db, err = sql.Open("mysql", dSN)
-	if err != nil {
-		log.Fatal("Could not connect to database")
-	}
+func main() {
+	log.Println(`I'm intended to interact with an sql server while handling
+		requests from the frontend.`)
 }
