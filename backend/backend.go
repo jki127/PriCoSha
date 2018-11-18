1d51c79f6d2ad49f7c109e45506688a2aac08447
gqo <ggmferguson@gmail.com>
2018-11-18 01:56:09 -0500

    Add basic backend interaction w/ mySQL DB Due to the change in structure from separate backend/frontend to changed package name in backend and imported it into frontend. Backend now features basic interaction with a mySQL database in the form of opening a connection to the database and pinging it.
    dSN stands for dataSourceName and that is what you call the "address" of the database that you must "dial" to connect to the database.
    The simplest dSN is in the form of [user]:[password]@/[databaseName].
    [user] and [password] are what you use to log in to phpmyadmin. [databaseName] is "pricosha" for our purposes.
    
    As we will be working on different local mySQL servers, we will need to enter different info for that to work.
    So, I wrote a simple config file reader that takes a .json file with your login info and the dbName (in case we change it) and creates the dSN from that.
    Currently, the file must be named config.json and must be located in the same directory as the backend (i.e. /backend).
    The file must look like this: https://i.imgur.com/SkhnT1u.png
    
    Additionally, I added a .gitignore file to ignore .json files in our repositories as I didn't want to track the config.json file.
    I may change this implementation if I think of something better.

--- backend/backend.go
+++ backend/backend.go
@@ -1,15 +1,47 @@
-// This is a placeholder backend .go file
-package main
+package backend
 
 import (
+	"database/sql"
+	"encoding/json"
 	"log"
+	"os"
 
-	_ "database/sql"
-
+	// Will be used to interact with the mysql DB
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
-	log.Println(`I'm intended to interact with an sql server while handling
-		requests from the frontend.`)
+// Conf holds configuration data for DSN
+type Conf struct {
+	User   string
+	Pass   string
+	DBName string
+}
+
+var db *sql.DB
+
+// TestDB tries to ping the database and returns the resulting error
+func TestDB() error {
+	return db.Ping()
+}
+
+func init() {
+	var configData Conf
+
+	configFile, err := os.Open("../backend/config.json")
+	if err != nil {
+		log.Println("Could not open config file.")
+	}
+	decoder := json.NewDecoder(configFile)
+	err = decoder.Decode(&configData)
+	if err != nil {
+		log.Println("Could not decode config file.")
+	}
+
+	dSN := configData.User + ":" + configData.Pass + "@/" + configData.DBName
+	log.Println(dSN)
+
+	db, err = sql.Open("mysql", dSN)
+	if err != nil {
+		log.Fatal("Could not connect to database")
+	}
 }
