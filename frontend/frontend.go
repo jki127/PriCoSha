6853d48ff0eb7a3ffe34bc157f6c94cf39924184
Andrea Vasquez <amv445@nyu.edu>
2018-11-19 15:14:32 -0500

    log out, switch cook value to empty

--- frontend/frontend.go
+++ frontend/frontend.go
@@ -160,4 +160,21 @@ func validateHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-
+// Handles requests to logout and deletes cookies accordingly
+func logoutHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := r.Cookie("username")
+	if err != nil {
+		log.Println("User was not logged in and cannot be logged out.")
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	} else {
+		cookie := http.Cookie{
+			Name:    "username",
+			Value:   "",
+			Expires: time.Unix(0, 0),
+		}
+		http.SetCookie(w, &cookie)
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	}
+}
