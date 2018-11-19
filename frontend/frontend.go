c5cf0de7e59975c88dea80bacf01820cca86b6a8
Andrea Vasquez <amv445@nyu.edu>
2018-11-19 14:08:29 -0500

    frontend side

--- frontend/frontend.go
+++ frontend/frontend.go
@@ -4,10 +4,52 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"time"
+
+	b "pricosha/backend"
 )
 
-// Port that server listens to http requests on (only edit number value)
+// Port that server listens to
 var httpPort = ":" + "8080"
+
+
+type MainPage struct {
+	LoggedOn   bool // true if logged in, false otherwise
+	Username string
+	PubData  []*b.ContentItem
+}
+
+func main() {
+	if b.TestDB() == nil {
+		log.Println("Database connected successfully!")
+	} else {
+		log.Fatal("Database could not be contacted.")
+	}
+
+	// Establish functions for handling requests to specific pages
+	http.HandleFunc("/", mainHandler)
+	http.HandleFunc("/favicon.ico", faviconHandler)
+	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/validate", validateHandler)
+	http.HandleFunc("/logout", logoutHandler)
+
+	// Start server
+	log.Println("Frontend spun up!")
+	err := http.ListenAndServe(httpPort, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe failed.")
+	}
+}
+
+// Handles requests to error pages
+func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
+	switch status {
+	case http.StatusNotFound:
+		http.ServeFile(w, r, "../web/static/not_found.html")
+	default:
+		http.ServeFile(w, r, "../web/static/unknown.html")
+	}
+}
 
 // Serves favicon file to favicon requests from browser
 func faviconHandler(w http.ResponseWriter, r *http.Request) {
@@ -21,29 +63,126 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
 		errorHandler(w, r, http.StatusNotFound)
 		return
 	}
-	/*
-		Template format is used as main is planned as templated page,
-		as such nil is passed to t.Execute()
-	*/
+
+	// Currently prints whether user is logged in or not.
+	cookie, err := r.Cookie("username")
+	var logged bool
+	var username string
+	if err != nil {
+		logged = false
+		username = ""
+	} else {
+		logged = true
+		username = cookie.Value
+	}
+
+	CurrentMPD := MPD{
+		Logged:   logged,
+		Username: username,
+		PubData:  b.GetPubContent(),
+	}
+
+	// data := b.GetPubContent()
 	t := template.Must(template.ParseFiles("../web/template/main.html"))
-	t.Execute(w, nil)
+	t.Execute(w, CurrentMPD)
 }
 
-func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
-	switch status {
-	case http.StatusNotFound:
-		http.ServeFile(w, r, "../web/static/not_found.html")
-	default:
-		http.ServeFile(w, r, "../web/static/unknown.html")
+// Handles requests to login page
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := r.Cookie("username")
+	if err == nil {
+		// User is already logged in, redirect
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	}
+
+	// Check if there was a previous login error
+	cookie, err := r.Cookie("logErr")
+	var errMsg string
+	var isErr bool
+	if err == nil {
+		isErr = true
+		if cookie.Value == "empty" {
+			errMsg = "Data fields were left empty. Please retry."
+		} else if cookie.Value == "fail" {
+			errMsg = "Incorrect login data. Please retry."
+		}
+	} else {
+		isErr = false
+	}
+
+	data := struct {
+		IsErr  bool
+		ErrMsg string
+	}{
+		isErr,
+		errMsg,
+	}
+
+	t := template.Must(template.ParseFiles("../web/template/login.html"))
+	t.Execute(w, data)
+}
+
+// Handles requests to validate user data and sets cookies accordingly
+func validateHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := r.Cookie("username")
+	if err == nil {
+		// User is already logged in, redirect
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	}
+
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	// This check should be revised later
+	if username == "" || password == "" {
+		cookie := http.Cookie{Name: "logErr", Value: "empty"}
+		http.SetCookie(w, &cookie)
+		http.Redirect(w, r, "/login", http.StatusFound)
+		return
+	}
+	if ok := b.ValidateInfo(username, password); ok {
+		log.Println("User logged in with:", username, password)
+		// Set cookie with user info
+		cookie := http.Cookie{Name: "username", Value: username}
+		http.SetCookie(w, &cookie)
+		// Delete logErr cookie if it exists
+		_, err := r.Cookie("logErr")
+		if err == nil {
+			c := http.Cookie{
+				Name:    "logErr",
+				Value:   "",
+				Expires: time.Unix(0, 0),
+			}
+			http.SetCookie(w, &c)
+		}
+
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	} else {
+		log.Println("User failed to log in with:", username, password)
+		cookie := http.Cookie{Name: "logErr", Value: "fail"}
+		http.SetCookie(w, &cookie)
+		http.Redirect(w, r, "/login", http.StatusFound)
+		return
 	}
 }
 
-func main() {
-	// Establish functions for handling requests to specific pages
-	http.HandleFunc("/", mainHandler)
-	http.HandleFunc("/favicon.ico", faviconHandler)
-
-	// Start server
-	log.Println("Frontend spun up!")
-	log.Fatal(http.ListenAndServe(httpPort, nil))
+// Handles requests to logout and deletes cookies accordingly
+func logoutHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := r.Cookie("username")
+	if err != nil {
+		log.Println("User was not logged in and cannot be logged out.")
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	} else {
+		cookie := http.Cookie{
+			Name:    "username",
+			Value:   "",
+			Expires: time.Unix(0, 0),
+		}
+		http.SetCookie(w, &cookie)
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	}
 }
