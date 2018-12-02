1f9cb00c1c216556c8e3d61717ae121869fc8af3
gqo <ggmferguson@gmail.com>
2018-12-02 18:34:23 -0500

    Change handler organization of frontend I changed the organization of how we add handlers to frontend to make it more modular and thus less prone to merge conflicts and other unfortunate errors. Every handler now is to be declared in a separate go file with its name followed by _handler. This is exactly like how it is when we declare handlers in the frontend.go file already. (i.e. validateLoginHandler -> validate_login_handler.go). These separate handlers should feature nothing but: a package main line, imports, any handler specific functions or data used, and the handler function itself. I also adapted a makefile that I found online to use in this project to help us with running this new organization. How do you use the makefile to run our frontend? It's relatively simple. Type "make run" in your terminal while in the /frontend directory to build all the .go files and run the program. Hit Ctrl+C to stop. You can also type "make" to build all the files into a binary if you want to just check compilation without running the program. If you want to clear the binary file that "make" creates in your directory, simply type "make clean" to delete it.

---
+++ frontend/favicon_handler.go
@@ -0,0 +1,8 @@
+package main
+
+import "net/http"
+
+// Serves favicon file to favicon requests from browser
+func faviconHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "../assets/images/favicon_hearts.ico")
+}
