38f399469fea094b2e45b91bc66b3da2fb2bef23
Jayson Isaac <jayson.isaac1@gmail.com>
2018-12-03 16:50:52 -0500

    Add ContentItems to SQL using NOW() as timestamp

--- sql/pricosha.sql
+++ sql/pricosha.sql
@@ -25,7 +25,7 @@ create table Content_Item
         on delete set null
     );
 
-    
+
 create table Friend_Group
     (fg_name varchar(32),
     owner_email varchar(64),
@@ -40,7 +40,7 @@ create table Tag
     (tagger_email varchar(64),
     tagged_email varchar(64),
     item_id int,
-    status boolean, 
+    status boolean,
     tag_time timestamp,
     primary key (tagger_email, tagged_email, item_id),
     foreign key (tagger_email) references Person(email)
@@ -50,7 +50,7 @@ create table Tag
     foreign key (item_id) references Content_Item(item_id)
         on delete cascade
     );
-    
+
 create table Rate
     (email varchar(64),
     item_id int,
@@ -74,10 +74,10 @@ create table Share
     foreign key (item_id) references Content_Item(item_id)
         on delete cascade
     );
-    
-    
+
+
 create table Belong
-    (member_email varchar(64), 
+    (member_email varchar(64),
     fg_name varchar(32),
     owner_email varchar(64),
     primary key (member_email, fg_name, owner_email),
@@ -86,7 +86,7 @@ create table Belong
     foreign key (fg_name, owner_email) references Friend_Group(fg_name, owner_email)
         on delete cascade
     );
-    
+
 -- Insert dummy data
 INSERT INTO Person VALUES ("AA@nyu.edu", SHA2("AA",256), "Ann", "Anderson");
 INSERT INTO Person VALUES ("BB@nyu.edu", SHA2("BB",256),"Bob", "Baker");
@@ -123,3 +123,9 @@ INSERT INTO Tag VALUES ("BB@nyu.edu", "FF@nyu.edu", 3, TRUE, "2018-10-27 09:22:3
 INSERT INTO Rate VALUES ("EE@nyu.edu", 1, "2018-11-27 09:22:30", "0x1f61a");
 INSERT INTO Rate VALUES ("HH@nyu.edu", 2, "2018-07-17 04:22:30", "0x1f61a");
 INSERT INTO Rate VALUES ("CC@nyu.edu", 4, "2018-03-23 12:22:30", "0x1f61a");
+INSERT INTO content_item
+  (poster_email, file_path, file_name, post_time, is_pub)
+VALUES
+  ("HH@nyu.edu", "/home/data/pie.jpg", "Pie", NOW(), 1),
+  ("HH@nyu.edu", "/home/data/turkey.jpg", "Turkey", NOW(), 1),
+  ("HH@nyu.edu", "/home/data/mashed.jpg", "Mashed Potatoes", NOW(), 1)
