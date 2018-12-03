cd52832a7929978efdbee2fc453c62333d1c63ac
Anthony Taldone <at3089@nyu.edu>
2018-12-02 21:05:33 -0500

    Modify Handler to prevent other User's from declining Someone's Tags

--- frontend/declineTagHandler.go
+++ frontend/declineTagHandler.go
@@ -1,35 +1,36 @@
 package main
 import (
-	// "log"
+	"log"
 	"net/http"
 	b "pricosha/backend"
 	"strconv"
 )
 
 func declineTagHandler(w http.ResponseWriter, r *http.Request){
-	// cookie, err := r.Cookie("username")
-	// // var logged bool
-	// var username string
-	// if err != nil {
-	// 	// logged = false
-	// 	username = ""
-	// 	log.Println("User was not logged in and cannot decline tags.")
-	// 	http.Redirect(w, r, "/login", http.StatusFound)
-	// 	return
-	// } else {
-	// 	// logged = true
-	// 	username = cookie.Value
-	// }
-
+	cookie, err := r.Cookie("username")
+	// var logged bool
+	var username string
+	if err != nil {
+		// logged = false
+		username = ""
+		log.Println("User was not logged in and cannot manage tags.")
+		http.Redirect(w, r, "/login", http.StatusFound)
+		return
+	} else {
+		// logged = true
+		username = cookie.Value
+	}	
 
 	url := r.URL
 	queryData :=url.Query()
 	id, _:=strconv.Atoi(queryData["iid"][0])
 
-	// if username==queryData["ted"][0] {
-	// 	b.DeclineTag(queryData["ter"][0],queryData["ted"][0],id)
-	// }
-	b.DeclineTag(queryData["ter"][0],queryData["ted"][0],id)
-
+	if username==queryData["ted"][0] {
+		b.DeclineTag(queryData["ter"][0],queryData["ted"][0],id)
+		
+	} else{
+		println("Cannot Manage other User's Tags")
+	}
+	
 	http.Redirect(w, r, "/login", http.StatusFound)
 }
