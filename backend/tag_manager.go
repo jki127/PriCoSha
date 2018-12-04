4b31cd52224094a5846ff39d46e756c2247c4c33
Anthony Taldone <at3089@nyu.edu>
2018-12-03 23:45:17 -0500

    Added getTagRows() for better code reuse

--- backend/tag_manager.go
+++ backend/tag_manager.go
@@ -1,6 +1,7 @@
 package backend
 
 import (
+	"database/sql"
 	"log"
 
 	// Used to interact with mySQL DB
@@ -15,36 +16,38 @@ type Tag struct {
 	TagTime     string
 	FileName    string
 	FilePath    string
-	TagID       int
+}
+
+//getTagRows Queries the database to find rows in which the user is tagged and the status matches the given parameter
+func getTagRows(status bool, username string) *sql.Rows {
+	rows, err := db.Query(`SELECT tagger_email, tagged_email, item_id, tag_time, file_name, file_path 
+		FROM Tag NATURAL JOIN Content_Item
+		WHERE status=?
+		AND tagged_email=?`,
+		status, username)
+	if err != nil {
+		log.Println(`backend: getTagRows(): Could not
+		query tags content from DB.`)
+	}
+	return rows
 }
 
 //GetPendingTags Gathers the Pending tags a user has by searching for tags with status=false, where the user is the Tagged person
 func GetPendingTags(username string) []*Tag {
-	rows, err := db.Query(`SELECT tagger_email, tagged_email, item_id, tag_time, file_name, file_path 
-		FROM Tag NATURAL JOIN Content_Item
-		WHERE status = false
-		AND tagged_email=?`,
-		username)
-	if err != nil {
-		log.Println(`backend: GetPendingTags(): Could not
-		query tags content from DB.`)
-	}
+	rows := getTagRows(false, username)
 	defer rows.Close()
 
 	var data []*Tag
-	tagCounter := 0
 	for rows.Next() {
 		var CurrentTag Tag
-		err = rows.Scan(&CurrentTag.TaggerEmail, &CurrentTag.TaggedEmail,
+		err := rows.Scan(&CurrentTag.TaggerEmail, &CurrentTag.TaggedEmail,
 			&CurrentTag.ItemID, &CurrentTag.TagTime,
 			&CurrentTag.FileName, &CurrentTag.FilePath)
 		if err != nil {
 			log.Println(`backend: GetPendingTags(): Could not scan row data
 			from tag content query.`)
 		}
-		CurrentTag.TagID = tagCounter
 		data = append(data, &CurrentTag)
-		tagCounter++
 	}
 
 	return data
@@ -52,22 +55,14 @@ func GetPendingTags(username string) []*Tag {
 
 //GetAcceptedTags Gathers the Pending tags a user has by searching for tags with status=true, where the user is the Tagged person
 func GetAcceptedTags(username string) []*Tag {
-	rows, err := db.Query(`SELECT tagger_email, tagged_email, item_id, tag_time, file_name, file_path 
-		FROM Tag NATURAL JOIN Content_Item
-		WHERE status = true
-		AND tagged_email=?`,
-		username)
-	if err != nil {
-		log.Println(`backend: AcceptedTags(): Could not
-		query tags content from DB.`)
-	}
+	rows := getTagRows(true, username)
 	defer rows.Close()
 
 	var data []*Tag
 
 	for rows.Next() {
 		var CurrentTag Tag
-		err = rows.Scan(&CurrentTag.TaggerEmail, &CurrentTag.TaggedEmail,
+		err := rows.Scan(&CurrentTag.TaggerEmail, &CurrentTag.TaggedEmail,
 			&CurrentTag.ItemID, &CurrentTag.TagTime,
 			&CurrentTag.FileName, &CurrentTag.FilePath)
 		if err != nil {
