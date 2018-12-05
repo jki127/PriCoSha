a86e6f0bc5c4d4d83b43a992ba473f3ef5a0cff0
Andrea Vasquez <amv445@nyu.edu>
2018-12-05 16:41:21 -0500

    basic structure for delete friend

---
+++ frontend/delete_friend_handler.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	b "pricosha/backend"
+)
+
+func deleteFriendHandler(w http.ResponseWriter, r *http.Request){
+	memEmail := r.Form("email")
+
+	url := r.URL
+	redirectStr := "/formDeleteFriend?" + url.RawQuery
+
+	if memEmail == "" {
+		cookie := http.Cookie{Name: "deleteFriendErr", Value: "empty"}
+		http.SetCookie(w, &cookie)
+		http.Redirect(w, r, redirectStr, http.StatusFound)
+		return
+	}
+
+	queryData := url.Query()
+	fgName := queryData["fgn"][0]
+	ownerEmail := queryData["oe"][0]
+
+	if ok := b.ValidateBelongFriendGroup(memEmail, fgName, ownerEmail); !ok { ///checks to see if member belongs in friend group to delete
+		b.DeleteFriend(memEmail, fgName, ownerEmail)
+		log.Println("Deleted Person with email", memEmail)
+	}else{
+		cookie := http.Cookie{Name: "addFriendErr", Value: "nonexistent"}
+		http.SetCookie(w, &cookie)
+		http.Redirect(w, r, redirectStr, http.StatusFound)
+		return
+	}
+
+	http.Redirect(w, r, "/friendgroups", http.StatusFound)
+	return
+
+}
