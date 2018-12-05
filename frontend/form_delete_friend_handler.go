692ba0d92624c8a8673a5bc3c645654dce04b009
Andrea Vasquez <amv445@nyu.edu>
2018-12-05 16:57:05 -0500

    does form in same layout add frined

--- frontend/form_delete_friend_handler.go
+++ frontend/form_delete_friend_handler.go
@@ -6,5 +6,38 @@ import (
 )
 
 func formDeleteFriendHandler(w http.ResponseWriter, r *http.Request){
-	
+	url := r.URL
+	queryData := url.Query()
+
+	cookie, err := r.Cookie("deleteFriendErr")
+	var errMsg string
+	var isErr bool
+
+	if err == nil {
+		isErr = true
+		if cookie.Value == "empty" {
+			errMsg = "Data fields were left empty. Please retry."
+		} else if cookie.Value == "nonexistent" {
+			errMsg = "Person you are trying to add does not exist. Please retry."
+		}
+	} else {
+		isErr = false
+	}
+
+	fgName := queryData["fgn"][0]
+	ownerEmail := queryData["oe"][0]
+	data := struct {
+		IsErr      bool
+		ErrMsg     string
+		FGName     string
+		OwnerEmail string
+	}{
+		isErr,
+		errMsg,
+		fgName,
+		ownerEmail,
+	}
+
+	t := template.Must(template.ParseFiles("../web/template/delete_friend.html"))
+	t.Execute(w, data)
 }
