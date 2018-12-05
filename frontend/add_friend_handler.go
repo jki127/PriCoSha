c8c6aa4c6ffc8935d558b8f7b84ae792049adf46
Andrea Vasquez <amv445@nyu.edu>
2018-12-05 15:19:07 -0500

    added a addFriendErr cookie for if added person already is in belong table

--- frontend/add_friend_handler.go
+++ frontend/add_friend_handler.go
@@ -47,6 +47,11 @@ func addFriendHandler(w http.ResponseWriter, r *http.Request) {
 	if ok := b.ValidateBelongFriendGroup(userEmail, fgName, ownerEmail); ok {
 		b.AddFriend(userEmail, fgName, ownerEmail)
 		log.Println("Added Person with email", userEmail)
+	}else{
+		cookie := http.Cookie{Name: "addFriendErr", Value: "alreadyBelongs"}
+		http.SetCookie(w, &cookie)
+		http.Redirect(w, r, redirectStr, http.StatusFound)
+		return
 	}
 
 	http.Redirect(w, r, "/friendgroups", http.StatusFound)
