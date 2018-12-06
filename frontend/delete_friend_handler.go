aa9ba0800b78286d892f1dfdaa752bc5625aee2d
Andrea Vasquez <amv445@nyu.edu>
2018-12-05 20:01:45 -0500

    problem deleting cookies

--- frontend/delete_friend_handler.go
+++ frontend/delete_friend_handler.go
@@ -8,20 +8,22 @@ import (
 
 func deleteFriendHandler(w http.ResponseWriter, r *http.Request){
 	memberEmail := r.FormValue("memberEmail")
-
+	clearCookie(&w, r, "deleteFriendErr")
 	url := r.URL
 	redirectStr := "/formDeleteFriend?" + url.RawQuery
 
+	/*
 	if memberEmail == "" {
 		cookie := http.Cookie{Name: "deleteFriendErr", Value: "empty"}
 		http.SetCookie(w, &cookie)
 		http.Redirect(w, r, redirectStr, http.StatusFound)
 		return
-	}
-
+	} */
+	//clearCookie(&w, r, "deleteFriendErr")
 	queryData := url.Query()
 	fgName := queryData["fgn"][0]
 	ownerEmail := queryData["oe"][0]
+	
 
 	if ok := b.ValidateBelongFriendGroup(memberEmail, fgName, ownerEmail); !ok { ///checks to see if member belongs in friend group to delete
 		b.DeleteFriend(memberEmail, fgName, ownerEmail)
