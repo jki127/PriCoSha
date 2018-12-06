f6892de5821189e9b190e5c522508db05c1ecfc5
Andrea Vasquez <amv445@nyu.edu>
2018-12-06 13:48:46 -0500

    makes template handler for choosing which friend

---
+++ frontend/choose_added_friend_handler.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	b "pricosha/backend"
+
+)
+
+func chooseAddedFriendHandler(w http.ResponseWriter, r *http.Request){
+	url := r.URL
+	queryData := url.Query()
+
+	redirectStr := "/duplicateNames?" + url.RawQuery
+
+	fgName := queryData["fgn"][0]
+	ownerEmail := queryData["oe"][0]
+	userEmail := queryData["ter"][0]
+
+	if ok := b.ValidateBelongFriendGroup(userEmail, fgName, ownerEmail); ok {
+		b.AddFriend(userEmail, fgName, ownerEmail)
+		log.Println("Added Person with email", userEmail)
+	}else{
+		cookie := http.Cookie{Name: "addFriendErr", Value: "alreadyBelongs"}
+		http.SetCookie(w, &cookie)
+		http.Redirect(w, r, redirectStr, http.StatusFound)
+		return
+	}
+
+}
