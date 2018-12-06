3391d8f8cf3ea76ee642a2c95518fce0c84d3d2f
Andrea Vasquez <amv445@nyu.edu>
2018-12-05 20:27:00 -0500

    Trying to fix cookies from resetting

--- frontend/delete_friend_handler.go
+++ frontend/delete_friend_handler.go
@@ -7,19 +7,20 @@ import (
 )
 
 func deleteFriendHandler(w http.ResponseWriter, r *http.Request){
+	
 	memberEmail := r.FormValue("memberEmail")
-	clearCookie(&w, r, "deleteFriendErr")
+	log.Println(memberEmail)
 	url := r.URL
 	redirectStr := "/formDeleteFriend?" + url.RawQuery
 
-	/*
+	
 	if memberEmail == "" {
 		cookie := http.Cookie{Name: "deleteFriendErr", Value: "empty"}
 		http.SetCookie(w, &cookie)
 		http.Redirect(w, r, redirectStr, http.StatusFound)
 		return
-	} */
-	//clearCookie(&w, r, "deleteFriendErr")
+	} 
+	
 	queryData := url.Query()
 	fgName := queryData["fgn"][0]
 	ownerEmail := queryData["oe"][0]
@@ -35,6 +36,7 @@ func deleteFriendHandler(w http.ResponseWriter, r *http.Request){
 		return
 	}
 	clearCookie(&w, r, "deleteFriendErr")
+	log.Println(memberEmail)
 	http.Redirect(w, r, "/friendgroups", http.StatusFound)
 	return
 
