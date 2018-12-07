9667787a27a8e05c9114af652dee18e654e2ee2f
Anthony Taldone <at3089@nyu.edu>
2018-12-06 19:15:37 -0500

    Add FriendsList on Profile and Prototype Bio

--- backend/profile.go
+++ backend/profile.go
@@ -9,10 +9,15 @@ import (
 
 //GetProfileData queries DB for data of user
 func GetProfileData(username string) (fname string, lname string) {
+	// func GetProfileData(username string) (fname string, lname string, bio string) {
 	rows, err := db.Query(`SELECT f_name, l_name 
 	FROM Person 
 	WHERE email=?`,
 		username)
+	// rows, err := db.Query(`SELECT f_name, l_name, bio
+	// FROM Person
+	// WHERE email=?`,
+	// 	username)
 	if err != nil {
 		log.Println(`backend: getProfileData(): Could not
 		query Profile Data from DB.`, username)
@@ -21,10 +26,56 @@ func GetProfileData(username string) (fname string, lname string) {
 
 	for rows.Next() {
 		err := rows.Scan(&fname, &lname)
+		// err := rows.Scan(&fname, &lname, &bio)
 		if err != nil {
 			log.Println(`backend: getProfileData(): Could not scan row data
 			from Person content query.`, username)
 		}
 	}
 	return fname, lname
+	// return fname, lname, bio
+}
+
+type FriendStruct struct {
+	FriendFirstName string
+	FriendLastName  string
+	FriendUsername  string
+}
+
+func GetFriendsList(username string) []*FriendStruct {
+	rows, err := db.Query(`SELECT member_email, f_name, l_name 
+	FROM Belong JOIN Person ON Belong.member_email=Person.email
+	WHERE Belong.owner_email=?`,
+		username)
+
+	// Declare variables for processing data
+	var (
+		data []*FriendStruct
+	)
+	for rows.Next() {
+		var CurrentFriend FriendStruct
+		err = rows.Scan(&CurrentFriend.FriendUsername, &CurrentFriend.FriendFirstName, &CurrentFriend.FriendLastName)
+		if err != nil {
+			log.Println(`backend: GetFriendsList(): Could not scan row data
+				from friends in user's friendgroups query.`)
+		}
+		data = append(data, &CurrentFriend)
+	}
+
+	rows, err = db.Query(`SELECT owner_email, f_name, l_name 
+	FROM Belong JOIN Person ON Belong.owner_email=Person.email
+	WHERE member_email=?`,
+		username)
+
+	for rows.Next() {
+		var CurrentFriend FriendStruct
+		err = rows.Scan(&CurrentFriend.FriendUsername, &CurrentFriend.FriendFirstName, &CurrentFriend.FriendLastName)
+		if err != nil {
+			log.Println(`backend: GetFriendsList(): Could not scan row data
+					from friends in user's friendgroups query.`)
+		}
+		data = append(data, &CurrentFriend)
+	}
+
+	return data
 }
--- frontend/profile_handler.go
+++ frontend/profile_handler.go
@@ -32,32 +32,38 @@ func profileHandler(w http.ResponseWriter, r *http.Request) {
 	username := cookie.Value
 
 	firstName, lastName := b.GetProfileData(username)
+	// firstName, lastName, bio := b.GetProfileData(username)
 	fgd := b.GetFriendGroup(username)
 	bfgd := b.GetBelongFriendGroup(username)
 	pendingTags := b.GetPendingTags(username)
 	pubCont := b.GetPubContent()
 	privCont := b.GetUserContent(username)
+	friends := b.GetFriendsList(username)
 
 	CurrentPD := struct {
-		Logged             bool
-		Username           string
-		Fname              string
-		Lname              string
+		Logged   bool
+		Username string
+		Fname    string
+		Lname    string
+		//Bio                string
 		FriendGroups       []*b.FriendGroup
 		BelongFriendGroups []*b.FriendGroup
 		PendingTags        []*b.Tag
 		PublicItems        []*b.ContentItem
 		PrivateItems       []*b.ContentItem
+		FriendsList        []*b.FriendStruct
 	}{
 		logged,
 		username,
 		firstName,
 		lastName,
+		// bio,
 		fgd,
 		bfgd,
 		pendingTags,
 		pubCont,
 		privCont,
+		friends,
 	}
 
 	t := template.Must(template.New("").ParseFiles("../web/template/profile.html", "../web/template/base.html"))
